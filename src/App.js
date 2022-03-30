0be34d5d3aaeb623c75697c397ed10abf53b907e
Sameer Gautam <sameer.gautam9860@gmail.com>
2022-03-30 19:29:33 +0545

    lets commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-import React, { useState } from 'react';
-import './App.css';
-
-function App() {
-  const [count,setCount]=useState(0);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1> {count}</h1>
-       <button className="up" onClick={() =>{
-            setCount(count+1) 
-       }}>increment </button>
-       <button className="down" onClick={() =>{ setCount(count-1)  }}>decrement </button>
-       <button className="zero" onClick ={()=>{setCount(0)}}  > Set to zero</button>
-
-       
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'
+ const App =() => {
+   return (
+     <p>Hello World</p>
+   )
+ }
+  export default App;
